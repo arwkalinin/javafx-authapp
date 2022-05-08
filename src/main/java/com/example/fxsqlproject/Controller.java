d83a0c75894475fb802df56c10c81ec656ae92af
Alexander Kalinin <arw.kalinin@gmail.com>
2022-05-09 01:49:22 +0300

    second screen layout

--- src/main/java/com/example/fxsqlproject/Controller.java
+++ src/main/java/com/example/fxsqlproject/Controller.java
@@ -1,14 +1,29 @@
 package com.example.fxsqlproject;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button loginButton;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private AnchorPane passwordField;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    void initialize() {
+        loginButton.setOnAction(event -> {
+            System.out.println("You clicked on loginButton.");
+        });
     }
+
 }
