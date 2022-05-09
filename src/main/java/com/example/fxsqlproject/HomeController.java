34a31b105ff7f6eab6e346aed21f067537f3d748
Alexander Kalinin <arw.kalinin@gmail.com>
2022-05-09 16:33:46 +0300

    registration layout

---
+++ src/main/java/com/example/fxsqlproject/HomeController.java
@@ -0,0 +1,11 @@
+package com.example.fxsqlproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class HomeController {
+
+    @FXML
+    private AnchorPane passwordField;
+
+}
---
+++ src/main/java/com/example/fxsqlproject/RegisterController.java
@@ -0,0 +1,39 @@
+package com.example.fxsqlproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class RegisterController {
+
+    @FXML
+    private PasswordField addressField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private PasswordField passField;
+
+    @FXML
+    private AnchorPane passwordField;
+
+    @FXML
+    private Button registerNewAccountButton;
+
+    @FXML
+    private CheckBox sexFemaleCheckBox;
+
+    @FXML
+    private CheckBox sexMaleCheckBox;
+
+    @FXML
+    private PasswordField surnameField;
+
+    @FXML
+    private TextField usernameField;
+
+}
