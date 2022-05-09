1e468f813d47b950d552f831b94be60a7e132716
Alexander Kalinin <arw.kalinin@gmail.com>
2022-05-09 16:59:44 +0300

    Controller Update

--- src/main/java/com/example/fxsqlproject/Controller.java
+++ src/main/java/com/example/fxsqlproject/Controller.java
@@ -1,9 +1,15 @@
 package com.example.fxsqlproject;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
 
@@ -23,6 +29,23 @@ public class Controller {
     void initialize() {
         loginButton.setOnAction(event -> {
             System.out.println("You clicked on loginButton.");
+        });
+
+        registerButton.setOnAction(event -> {
+            registerButton.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("register-window.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
         });
     }
 
